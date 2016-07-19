f9fafa2c28c3257e6d27a1e668a51f314affe58b
java <java>
2016-07-19 16:23:26 +0900

    Helld Label add..

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -1,9 +1,10 @@
 package com.hybrid;
 public class MainApplication {
 public static void main(String[] args) {
-
+	System.out.println("###################");
 	System.out.println("###################");
 	System.out.println("Hello First Gradle");
+	System.out.println("###################");
 	System.out.println("###################");
 }
 }
